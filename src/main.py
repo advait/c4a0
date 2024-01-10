3e4fdc64c18f6a322656ba3516ac0d187dd3a134
Advait Shinde <advait.shinde@gmail.com>
2024-01-09 17:57:05 -0800

    Add argparse args.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
+from argparse import ArgumentParser
 import asyncio
 import logging
+import multiprocessing as mp
+from sys import platform
+
+import torch
 
 from training import train
 
@@ -11,8 +16,67 @@ async def main():
         level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
     )
 
+    device = get_torch_device()
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--n-games", type=int, default=100, help="number of games per generation"
+    )
+    parser.add_argument(
+        "--n-processes",
+        type=int,
+        default=mp.cpu_count() - 1,
+        help="number of processes to use for self-play",
+    )
+    parser.add_argument(
+        "--mcts-iterations",
+        type=int,
+        default=150,
+        help="number of MCTS iterations per move",
+    )
+    parser.add_argument(
+        "--exploration-constant",
+        type=float,
+        default=1.4,
+        help="MCTS exploration constant",
+    )
+    parser.add_argument(
+        "--batch-size", type=int, default=100, help="batch size for training"
+    )
+    parser.add_argument("--device", type=str, default=device, help="pytorch device")
+
+    args = parser.parse_args()
+
     while True:
-        await train()
+        await train(
+            n_games=args.n_games,
+            n_processes=args.n_processes,
+            mcts_iterations=args.mcts_iterations,
+            exploration_constant=args.exploration_constant,
+            batch_size=args.batch_size,
+            device=args.device,
+        )
+
+
+def get_torch_device() -> torch.device:
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+
+    if platform == "darwin":
+        if torch.backends.mps.is_available():
+            return torch.device("mps")
+        elif not torch.backends.mps.is_built():
+            raise RuntimeError(
+                "MPS not available because the current PyTorch install was not "
+                "built with MPS enabled."
+            )
+        else:
+            raise RuntimeError(
+                "MPS not available because the current MacOS version is not 12.3+ "
+                "and/or you do not have an MPS-enabled device on this machine."
+            )
+
+    return torch.device("cpu")
 
 
 if __name__ == "__main__":
