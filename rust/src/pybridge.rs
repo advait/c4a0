bb0536d265b7cad6a7a4e67f13412d80f1a266cf
Advait Shinde <advait.shinde@gmail.com>
2024-07-08 10:06:36 -0700

    Refactor pybridge.

--- rust/src/pybridge.rs
+++ rust/src/pybridge.rs
@@ -6,22 +6,9 @@ use pyo3::{prelude::*, types::PyList};
 
 use crate::{
     c4r::Pos,
-    mcts::Policy,
+    mcts::{Policy, Sample},
     self_play::{self_play, EvalPosResult, EvalPosT},
 };
-
-/// A batch of training samples.
-#[pyclass]
-pub struct PySamples {
-    player0_ids: Py<PyArray1<u64>>, // b
-    player1_ids: Py<PyArray1<u64>>, // b
-    pos: Py<PyArray4<f32>>,         // b, c, h, w
-    policy: Py<PyArray2<f32>>,      // b, w
-    value: Py<PyArray1<f32>>,       // b
-}
-
-#[pymethods]
-impl PySamples {}
 
 /// Generate training samples with self-play. This is a python wrapper around [self_play].
 #[pyfunction]
@@ -33,23 +20,18 @@ pub fn gen_samples<'py>(
     exploration_constant: f32,
     py_eval_pos_cb: &Bound<'py, PyAny>,
 ) -> PyResult<PySamples> {
-    if !py_eval_pos_cb.is_callable() {
-        panic!("py_eval_pos_cb must be callable");
-    }
-
     let reqs: Vec<(usize, usize, usize)> = reqs.extract().expect("error extracting reqs");
-    let batcher = PyEvalPos {
+    let eval_pos = PyEvalPos {
         py_eval_pos_cb: py_eval_pos_cb.to_object(py),
     };
 
     let samples = {
-        let batcher = batcher.clone();
         // Start background processing threads while releasing the GIL with allow_threads.
         // This allows other python threads (e.g. pytorch) to continue while we generate training
         // samples. When we need to call the py_eval_pos callback, we will re-acquire the GIL.
         py.allow_threads(move || {
             self_play(
-                batcher,
+                eval_pos,
                 reqs.len(),
                 max_nn_batch_size,
                 n_mcts_iterations,
@@ -58,94 +40,38 @@ pub fn gen_samples<'py>(
         })
     };
 
-    let ret = PySamples {
-        player0_ids: Array1::from_vec(vec![0; samples.len()])
-            .into_pyarray_bound(py)
-            .into(),
-        player1_ids: Array1::from_vec(vec![1; samples.len()])
-            .into_pyarray_bound(py)
-            .into(),
-        pos: batcher
-            .create_pos_batch(py, &samples.iter().map(|s| s.pos.clone()).collect())
-            .into(),
-        policy: batcher
-            .create_policy_batch(py, &samples.iter().map(|s| s.policy.clone()).collect())
-            .into(),
-        value: batcher
-            .create_value_batch(py, &samples.iter().map(|s| s.value).collect())
-            .into(),
-    };
-
-    Ok(ret)
+    Ok(PySamples::from_samples(py, samples))
 }
 
-#[derive(Clone)]
+/// A batch of training samples.
+#[pyclass]
+pub struct PySamples {
+    player0_ids: Py<PyArray1<u64>>, // b
+    player1_ids: Py<PyArray1<u64>>, // b
+    pos: Py<PyArray4<f32>>,         // b, c, h, w
+    policy: Py<PyArray2<f32>>,      // b, w
+    value: Py<PyArray1<f32>>,       // b
+}
+
+impl PySamples {
+    fn from_samples<'py>(py: Python<'py>, samples: Vec<Sample>) -> Self {
+        Self {
+            player0_ids: Array1::from_vec(vec![0; samples.len()])
+                .into_pyarray_bound(py)
+                .into(),
+            player1_ids: Array1::from_vec(vec![1; samples.len()])
+                .into_pyarray_bound(py)
+                .into(),
+            pos: create_pos_batch(py, &samples.iter().map(|s| s.pos.clone()).collect()).into(),
+            policy: create_policy_batch(py, &samples.iter().map(|s| s.policy.clone()).collect())
+                .into(),
+            value: create_value_batch(py, &samples.iter().map(|s| s.value).collect()).into(),
+        }
+    }
+}
+
 struct PyEvalPos {
     py_eval_pos_cb: PyObject,
-}
-
-impl PyEvalPos {
-    /// Creates a batch of positions in tensor format.
-    fn create_pos_batch<'py>(
-        &self,
-        py: Python<'py>,
-        positions: &Vec<Pos>,
-    ) -> Bound<'py, PyArray4<f32>> {
-        let mut buffer = vec![0.0; positions.len() * Pos::BUF_LEN];
-        for i in 0..positions.len() {
-            let pos = &positions[i];
-            let pos_buffer = &mut buffer[i * Pos::BUF_LEN..(i + 1) * Pos::BUF_LEN];
-            pos.write_numpy_buffer(pos_buffer);
-        }
-
-        Array4::from_shape_vec(
-            (
-                positions.len(),
-                Pos::BUF_N_CHANNELS,
-                Pos::N_ROWS,
-                Pos::N_COLS,
-            ),
-            buffer,
-        )
-        .expect("Failed to create Array4 from buffer")
-        .into_pyarray_bound(py)
-    }
-
-    /// Creates a batch of policies in tensor format.
-    fn create_policy_batch<'py>(
-        &self,
-        py: Python<'py>,
-        policies: &Vec<Policy>,
-    ) -> Bound<'py, PyArray2<f32>> {
-        let mut buffer = vec![0.0; policies.len() * Pos::N_COLS];
-        for i in 0..policies.len() {
-            let policy = &policies[i];
-            let policy_buffer = &mut buffer[i * Pos::N_COLS..(i + 1) * Pos::N_COLS];
-            policy_buffer.copy_from_slice(policy);
-        }
-
-        Array2::from_shape_vec((policies.len(), Pos::N_COLS), buffer)
-            .expect("Failed to create Array2 from buffer")
-            .into_pyarray_bound(py)
-    }
-
-    /// Creates a batch of position values in tensor format.
-    fn create_value_batch<'py>(
-        &self,
-        py: Python<'py>,
-        values: &Vec<f32>,
-    ) -> Bound<'py, PyArray1<f32>> {
-        Array1::from_shape_vec((values.len(),), values.clone())
-            .expect("Failed to create Array1 from buffer")
-            .into_pyarray_bound(py)
-    }
-
-    fn policy_from_slice(&self, policy: &[f32]) -> Policy {
-        debug_assert_eq!(policy.len(), Pos::N_COLS);
-        let mut ret = Policy::default();
-        ret.copy_from_slice(policy);
-        ret
-    }
 }
 
 impl EvalPosT for PyEvalPos {
@@ -155,7 +81,7 @@ impl EvalPosT for PyEvalPos {
     /// before performing the callback.
     fn eval_pos(&self, pos: &Vec<Pos>) -> Vec<EvalPosResult> {
         Python::with_gil(|py| {
-            let pos_batch = self.create_pos_batch(py, pos);
+            let pos_batch = create_pos_batch(py, pos);
 
             let (policy, value): (PyReadonlyArray2<f32>, PyReadonlyArray1<f32>) = (&self
                 .py_eval_pos_cb
@@ -169,10 +95,60 @@ impl EvalPosT for PyEvalPos {
 
             (0..pos.len())
                 .map(|i| EvalPosResult {
-                    policy: self.policy_from_slice(&policy[i * Pos::N_COLS..(i + 1) * Pos::N_COLS]),
+                    policy: policy_from_slice(&policy[i * Pos::N_COLS..(i + 1) * Pos::N_COLS]),
                     value: value[i],
                 })
                 .collect()
         })
     }
+}
+
+/// Creates a batch of positions in tensor format.
+fn create_pos_batch<'py>(py: Python<'py>, positions: &Vec<Pos>) -> Bound<'py, PyArray4<f32>> {
+    let mut buffer = vec![0.0; positions.len() * Pos::BUF_LEN];
+    for i in 0..positions.len() {
+        let pos = &positions[i];
+        let pos_buffer = &mut buffer[i * Pos::BUF_LEN..(i + 1) * Pos::BUF_LEN];
+        pos.write_numpy_buffer(pos_buffer);
+    }
+
+    Array4::from_shape_vec(
+        (
+            positions.len(),
+            Pos::BUF_N_CHANNELS,
+            Pos::N_ROWS,
+            Pos::N_COLS,
+        ),
+        buffer,
+    )
+    .expect("Failed to create Array4 from buffer")
+    .into_pyarray_bound(py)
+}
+
+/// Creates a batch of policies in tensor format.
+fn create_policy_batch<'py>(py: Python<'py>, policies: &Vec<Policy>) -> Bound<'py, PyArray2<f32>> {
+    let mut buffer = vec![0.0; policies.len() * Pos::N_COLS];
+    for i in 0..policies.len() {
+        let policy = &policies[i];
+        let policy_buffer = &mut buffer[i * Pos::N_COLS..(i + 1) * Pos::N_COLS];
+        policy_buffer.copy_from_slice(policy);
+    }
+
+    Array2::from_shape_vec((policies.len(), Pos::N_COLS), buffer)
+        .expect("Failed to create Array2 from buffer")
+        .into_pyarray_bound(py)
+}
+
+/// Creates a batch of position values in tensor format.
+fn create_value_batch<'py>(py: Python<'py>, values: &Vec<f32>) -> Bound<'py, PyArray1<f32>> {
+    Array1::from_shape_vec((values.len(),), values.clone())
+        .expect("Failed to create Array1 from buffer")
+        .into_pyarray_bound(py)
+}
+
+fn policy_from_slice(policy: &[f32]) -> Policy {
+    debug_assert_eq!(policy.len(), Pos::N_COLS);
+    let mut ret = Policy::default();
+    ret.copy_from_slice(policy);
+    ret
 }
