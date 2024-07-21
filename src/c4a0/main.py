79322310ee70365a221cc11f03998bf02372ffd2
Advait Shinde <advait.shinde@gmail.com>
2024-07-21 14:37:15 +0900

    Eliminate tournament from training loop.

--- src/c4a0/main.py
+++ src/c4a0/main.py
@@ -8,29 +8,32 @@ import clipstick
 from pydantic import BaseModel
 import torch
 
-from c4a0.training import TrainingState
+from c4a0.training import training_loop
 from c4a0.utils import get_torch_device
 
 
 class Train(BaseModel):
     """Trains a model via self-play."""
 
-    n_games: int = 2000
+    n_self_play_games: int = 2000
     """number of games per generation"""
 
-    batch_size: int = 2000
+    self_play_batch_size: int = 2000
+    """batch size for self play inference"""
+
+    training_batch_size: int = 2000
     """batch size for training"""
 
     def run(self, args: "MainArgs"):
-        state = TrainingState.load_training_state()
-        while True:
-            state.continue_training(
-                n_games=self.n_games,
-                mcts_iterations=args.mcts_iterations,
-                exploration_constant=args.exploration_constant,
-                batch_size=self.batch_size,
-                device=torch.device(args.device),
-            )
+        training_loop(
+            base_dir="training",
+            device=torch.device(args.device),
+            n_self_play_games=self.n_self_play_games,
+            n_mcts_iterations=args.n_mcts_iterations,
+            exploration_constant=args.exploration_constant,
+            self_play_batch_size=self.self_play_batch_size,
+            training_batch_size=self.training_batch_size,
+        )
 
 
 class Tournament(BaseModel):
@@ -45,7 +48,7 @@ class MainArgs(BaseModel):
 
     sub_command: Train | Tournament
 
-    mcts_iterations: int = 150
+    n_mcts_iterations: int = 150
     """number of MCTS iterations per move"""
 
     exploration_constant: float = 1.4
