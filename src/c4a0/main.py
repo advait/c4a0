ccc5bc3ed6d69f18a0de1e0c529b7061e01d3f29
Advait Shinde <advait.shinde@gmail.com>
2024-08-27 10:09:40 -0700

    Replace wandb with optuna:
      - Create new mcts-sweep command for sweeping mcts values
      - Rename old sweep to nn-sweep

--- src/c4a0/main.py
+++ src/c4a0/main.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
+from pathlib import Path
 from typing import List
 import warnings
 
+from loguru import logger
+import optuna
 import torch
 import typer
 
 from c4a0.nn import ModelConfig
-from c4a0.sweep import perform_sweep
+from c4a0.sweep import perform_hparam_sweep
 from c4a0.tournament import ModelID, RandomPlayer, UniformPlayer
-from c4a0.training import TrainingGen, training_loop
+from c4a0.training import SolverConfig, TrainingGen, parse_lr_schedule, training_loop
 from c4a0.utils import get_torch_device
 
 import c4a0_rust  # type: ignore
@@ -35,22 +38,12 @@ def train(
     l2_reg: float = 4e-4,
 ):
     """Trains a model via self-play."""
-    assert len(lr_schedule) % 2 == 0, "lr_schedule must have an even number of elements"
-    schedule = {}
-    for i in range(0, len(lr_schedule), 2):
-        threshold = int(lr_schedule[i])
-        assert (
-            threshold == lr_schedule[i]
-        ), "lr_schedule must alternate between gen_id (int) and lr (float)"
-        lr = lr_schedule[i + 1]
-        schedule[threshold] = lr
-
     model_config = ModelConfig(
         n_residual_blocks=n_residual_blocks,
         conv_filter_size=conv_filter_size,
         n_policy_layers=n_policy_layers,
         n_value_layers=n_value_layers,
-        lr_schedule=schedule,
+        lr_schedule=parse_lr_schedule(lr_schedule),
         l2_reg=l2_reg,
     )
     training_loop(
@@ -64,12 +57,6 @@ def train(
         training_batch_size=training_batch_size,
         model_config=model_config,
     )
-
-
-@app.command()
-def sweep(base_dir: str = "training"):
-    """Perofrms a hyperparameter sweep."""
-    perform_sweep(base_dir)
 
 
 @app.command()
@@ -97,6 +84,90 @@ def ui(
         c_exploration,
         c_ply_penalty,
     )
+
+
+@app.command()
+def nn_sweep(base_dir: str = "training"):
+    """
+    Performs a hyperparameter sweep to determine best nn model params based on existing training
+    data.
+    """
+    perform_hparam_sweep(base_dir)
+
+
+@app.command()
+def mcts_sweep(
+    device: str = str(get_torch_device()),
+    c_ply_penalty: float = 0.01,
+    self_play_batch_size: int = 2000,
+    training_batch_size: int = 2000,
+    n_residual_blocks: int = 1,
+    conv_filter_size: int = 32,
+    n_policy_layers: int = 4,
+    n_value_layers: int = 2,
+    lr_schedule: List[float] = [0, 2e-3],
+    l2_reg: float = 4e-4,
+    base_training_dir: str = "training-sweeps",
+    optuna_db_path: str = "optuna.db",
+    n_trials: int = 100,
+    max_gens_per_trial: int = 10,
+    solver_path: str = "/home/advait/connect4/c4solver",
+    book_path: str = "/home/advait/connect4/7x6.book",
+    solutions_path: str = "./solutions.db",
+):
+    """
+    Performs sweep of MCTS hyperparameters (e.g. n_self_play_games, n_mcts_iterations,
+    c_exploration) to determine optimal values by performing `n_trials` independent training
+    runs, each with `max_gens_per_trial` generations, seeking to maximize the solver score.
+    """
+    base_path = Path(base_training_dir)
+    base_path.mkdir(exist_ok=True)
+
+    model_config = ModelConfig(
+        n_residual_blocks=n_residual_blocks,
+        conv_filter_size=conv_filter_size,
+        n_policy_layers=n_policy_layers,
+        n_value_layers=n_value_layers,
+        lr_schedule=parse_lr_schedule(lr_schedule),
+        l2_reg=l2_reg,
+    )
+
+    def objective(trial: optuna.Trial):
+        trial_path = base_path / f"trial_{trial.number}"
+        trial_path.mkdir(exist_ok=False)
+        gen = training_loop(
+            base_dir=str(trial_path),
+            device=torch.device(device),
+            n_self_play_games=trial.suggest_int("n_self_play_games", 1000, 5000),
+            n_mcts_iterations=trial.suggest_int("n_mcts_iterations", 100, 1500),
+            c_exploration=trial.suggest_float("c_exploration", 0.5, 12.0),
+            c_ply_penalty=c_ply_penalty,
+            self_play_batch_size=self_play_batch_size,
+            training_batch_size=training_batch_size,
+            model_config=model_config,
+            max_gens=max_gens_per_trial,
+            solver_config=SolverConfig(
+                solver_path=solver_path,
+                book_path=book_path,
+                solutions_path=solutions_path,
+            ),
+        )
+        logger.info(
+            "Trial {} completed. Solver score: {}", trial.number, gen.solver_score
+        )
+        score = gen.solver_score
+        assert score is not None
+        return score
+
+    storage_name = f"sqlite:///{optuna_db_path}"
+    study = optuna.create_study(
+        study_name="mcts_sweep",
+        storage=storage_name,
+        load_if_exists=True,
+        direction="maximize",
+        pruner=optuna.pruners.MedianPruner(),
+    )
+    study.optimize(objective, n_trials=n_trials)
 
 
 @app.command()
